03543af9e691ed5408be6dbf45c2c1f8bafe1e77
KombinatorVit <slonik0775@gmail.com>
2023-06-29 11:36:17 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+          <span style={{color: 'white'}}> Start project!</span>
+    </div>
+  );
+}
+
+export default App;
